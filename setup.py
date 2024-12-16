6e853af28ad79c51bb8af7ca0815ccd746cbe3ea
Davis Raymond <davis.ray.muro@gmail.com>
2024-12-15 17:20:44 -0800

    drop support for python 3.7 (#22)
    * drop support for python 3.7
    * revert version change
    * add more classifiers
    * remove `pip-tools` dev dependency

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
     author_email="davisraymondmuro@outlook.com",
     packages=["django_filters-stubs"],
     package_data={"django_filters-stubs": find_stub_files("django_filters-stubs")},
+    python_requires=">=3.8",
     install_requires=[
         "mypy>=0.750",
         "django-stubs>=1.3.0",
@@ -42,6 +43,9 @@ setup(
     classifiers=[
         "Development Status :: 1 - Planning",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
 )
