0c6a183040b888487bc2da0dd0990729aab75e0f
Davis Raymond Muro <davis.ray.muro@gmail.com>
2024-12-15 16:12:46 -0800

    switch to using `uv`
    - Use pyproject.toml and setup tools as the build backend

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ name = "django-filter-stubs"
 
 def find_stub_files(name):
     result = []
-    for root, dirs, files in os.walk(name):
+    for root, _, files in os.walk(name):
         for f in files:
             if f.endswith(".pyi"):
                 if os.path.sep in root:
@@ -22,26 +22,6 @@ long_description = (Path(__file__).parent / "README.md").read_text()
 
 
 setup(
-    name="django-filter-stubs",
-    version="0.1.3",
-    description="PEP-484 stubs for django-filter",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    license="MIT",
-    url="https://github.com/DavisRayM/django-filter-stubs",
-    author="Davis Raymond Muro",
-    author_email="davisraymondmuro@outlook.com",
-    packages=['django_filters-stubs'],
-    package_data={"django_filters-stubs": find_stub_files('django_filters-stubs')},
-    install_requires=[
-        "mypy>=0.750",
-        "django-stubs>=1.3.0",
-        "djangorestframework-stubs>=0.4.0",
-        "typing-extensions>=3.7.4",
-    ],
-    classifiers=[
-        "Development Status :: 1 - Planning",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.7",
-    ],
+    packages=["django_filters-stubs"],
+    package_data={"django_filters-stubs": find_stub_files("django_filters-stubs")},
 )
